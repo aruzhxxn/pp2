6e4c02ccf3a455bfc85e3ca29ffad4b0dc71c4a7
aruzhxxn <aruzhxxn@gmail.com>
2026-01-31 02:19:56 +0500

    add examples

---
+++ Practices/Practice_1/python-basics/9.py
@@ -0,0 +1,19 @@
+# ex 1: declare a string, and read the 4-th value
+x = "Furina"
+print(x[3])
+
+# ex 2: finding the size (length) of a string
+x = "Hello, Furina!"
+print(len(x)) # <-- commas, spaces and all symbols are counted
+
+#  ex 3: read the 1-st value
+x = "Furina"
+print(x[0])
+
+# ex 4: if the word exist in string:
+x = "Hello, Furina!"
+print("Furina" in x) # bool type
+
+# ex 5: if the word is not exist, that is the reverse of the ex#4
+x = "Hello, Furina"
+print("World" not in x)
