784fadcf2aa290e73e944342e7b806b82ad004b2
aruzhxxn <aruzhxxn@gmail.com>
2026-02-07 13:09:19 +0500

    Update for_break.py

--- Practice/practice2/loops/for_break.py
+++ Practice/practice2/loops/for_break.py
@@ -2,13 +2,13 @@
 archons_list = ["venti", "zhongli", "raiden", "nahida", "furina", "mavuika", "mualani"]
 for archon in archons_list:
     print(archon)
-    if (archon == "Furina"):
+    if (archon == "furina"):
         break
 
 
 # ex 2: break the loop but not inclusively
 archons_list = ["venti", "zhongli", "raiden", "nahida", "furina", "mavuika", "mualani"]
 for archon in archons_list:
-    if archon == "Mavuika": # 1 step after
+    if archon == "mavuika": # 1 step after
         break
     print(archon)
